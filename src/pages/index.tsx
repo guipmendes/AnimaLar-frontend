b2ecc9a3d34d1c5996805f3aea8b568400d06e8d
avrcoelho <andrevrcoelho@hotmail.com>
2021-06-19 13:48:18 -0300

    feat: backgorund component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import { Background } from "@/src/components/Background";
 
+import styles from "@/src/styles/home.module.scss";
+
 export default function Home() {
   return (
-    <>
+    <main className={styles.container}>
       <Background />
-    </>
+
+      <section className={styles.formContainer}></section>
+    </main>
   );
 }
